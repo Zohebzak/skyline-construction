d7409ffbf3afc365759678413d68f68455a2f284
mubashir.khan <mubashirkhan57510@gmail.com>
2023-05-30 06:47:51 -0700

    fixdkdf

--- src/components/loginpage/Login.js
+++ src/components/loginpage/Login.js
@@ -6,13 +6,26 @@ import loginPic from '../../images/our_vision2.jpg'
 
 function Login() {
     // const [isLogin, setIsLogin] = useState(true)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleOnClick = (e) => {
+        e.preventDefault();
+        console.log(email);
+        console.log(password);
+        const payLoad={
+            email:email,
+            password:password
+        }
+        console.log(payLoad);
+    }
     const navigate = useNavigate()
     return (
         <>
 
             <div>
                 <div className='mai_div'>
-                    <div className='login-pic'>
+                    <div className='login-pic' style={{ marginLeft: '2rem' }}>
                         <img src={loginPic} />
                     </div>
                     <div className='login-form'>
@@ -23,11 +36,11 @@ function Login() {
                             <h3 className='text_area'>Skyline Construction PVT LTD</h3>
                         </div>
                         <div className='inputs'>
-                            <input type='email' placeholder='Email Id' className='input-items' />
-                            <input type='password' placeholder='Password' className='input-items' />
+                            <input type='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email Id' className='input-items' />
+                            <input type='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' className='input-items' />
                         </div>
                         <div className='sing_btn'>
-                            <button>Login</button>
+                            <button onClick={handleOnClick}>Login</button>
                         </div>
                     </div>
                 </div>
